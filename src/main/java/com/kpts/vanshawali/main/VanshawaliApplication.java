aa7ce323d5446f00e34953d9797a72e495bfe848
vineetmishra82 <60233390+vineetmishra82@users.noreply.github.com>
2023-04-16 23:43:18 +0530

    till cors

--- src/main/java/com/kpts/vanshawali/main/VanshawaliApplication.java
+++ src/main/java/com/kpts/vanshawali/main/VanshawaliApplication.java
@@ -2,15 +2,31 @@ package com.kpts.vanshawali.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.kpts.vanshawali.controller","com.kpts.vanshawali.service"})
 @EnableMongoRepositories("com.kpts.vanshawali.repositories")
 public class VanshawaliApplication {
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() 
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+            	registry.addMapping("/**")
+                .allowedMethods("*");            	
+            	
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(VanshawaliApplication.class, args);
